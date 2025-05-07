c9b0584dafdeb77f36e42a45d90d075275dd31af
Valentyn Kasyan <vkasyan@gmx.de>
2025-05-07 23:13:52 +0200

    git checkout -b 08.03 trpc-integraton-prefetch

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,48 +1,60 @@
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
+// import configPromise from "@payload-config";
+// import { getPayload } from "payload";
+
+import { Suspense } from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
-import { Category } from "../../../../payload-types";
-import { CustomCategory } from "./types";
+// import { Category } from "../../../../payload-types";
+// import { CustomCategory } from "./types";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = async ({ children }: Props) => {
-  const payload = await getPayload({
-    config: configPromise,
-  });
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.categories.getMany.queryOptions());
 
-  const data = await payload.find({
-    collection: "categories",
-    depth: 1, // populate subcategories, subcategories.[0] will be a type of "Category"
-    pagination: false,
-    where: {
-      parent: {
-        exists: false,
-      },
-    },
-    sort: "name",
-  });
+  // const payload = await getPayload({
+  //   config: configPromise,
+  // });
 
-  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
-    ...doc,
-    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-      // Because of "depth: 1" we are confident doc will be a type of category
-      ...(doc as Category),
-      subcategories: undefined,
-    })),
-  }));
+  // const data = await payload.find({
+  //   collection: "categories",
+  //   depth: 1, // populate subcategories, subcategories.[0] will be a type of "Category"
+  //   pagination: false,
+  //   where: {
+  //     parent: {
+  //       exists: false,
+  //     },
+  //   },
+  //   sort: "name",
+  // });
+
+  // const formattedData: CustomCategory[] = data.docs.map((doc) => ({
+  //   ...doc,
+  //   subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+  //     // Because of "depth: 1" we are confident doc will be a type of category
+  //     ...(doc as Category),
+  //     subcategories: undefined,
+  //   })),
+  // }));
 
   // console.log({data, formattedData});
 
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <SearchFilters data={formattedData} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <SearchFilters />
+        </Suspense>
+      </HydrationBoundary>
+
       <div className="flex-1 bg-[#F4F4F0]"> {children}</div>
       <Footer />
     </div>
--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -1,14 +1,19 @@
-// src/app/(app)/(home)/search-filters/index.tsx
+"use client";
 
-import { CustomCategory } from "../types";
+import { useTRPC } from "@/trpc/client";
+// import { CustomCategory } from "../types";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
-interface Props {
-  data: CustomCategory[];
-}
+// interface Props {
+//   data: CustomCategory[];
+// }
 
-export const SearchFilters = ({ data }: Props) => {
+export const SearchFilters = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
       {/* Pass data to Search Input for showing the categoriesSidebar  */}
