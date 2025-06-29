3cf802bcd79340096e53e2638712581210639d67
Valentyn Kasyan <vkasyan@gmx.de>
2025-06-29 18:47:35 +0200

    fix: update postcss config for Tailwind v4 and Vercel

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,12 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+// change the file for vercel deployment alligned with Tailwind v4.:
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: ["@tailwindcss/postcss", "autoprefixer"],
 };
 
 export default config;
